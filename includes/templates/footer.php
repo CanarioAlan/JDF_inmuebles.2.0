29c8ef671d9a8e2460e06a0a5b6ee61c9d366d3d
Canario Alan <alan.union.1998@gmail.com>
2024-06-05 17:59:22 -0300

    agregando el php y el css de contacto

---
+++ includes/templates/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer">
+        <section class="footer-nav">
+            <a class="footer__navegacion-links" href="index.html">inicio</a>
+            <a class="footer__navegacion-links" href="ventas.html">Ventas</a>
+            <a class="footer__navegacion-links" href="alquileres.html">alquileres</a>
+            <a class="footer__navegacion-links" href="contacto.html">contacto</a>
+        </section>
+        <section class="footer__informacion">
+            <div class="footer__redes">
+                <h2 class="footer__infomarcion-titulo">Encontranos en:</h2>
+                <div class="footer__redes__links">
+                    <a href="https://www.facebook.com/JDFinmuebles"
+                        class="redes__sociales-facebook footer__links-info">Facebook</a>
+                    <a href="https://www.instagram.com/jdfinmuebles/"
+                        class="redes__sociales-instagram footer__links-info">Instagram</a>
+                    <a href="mailto:info@jdfinmuebles.com" class="redes__sociales-email footer__links-info">E-mail</a>
+                </div>
+            </div>
+        </section>
+        <section class="footer__ubicacion">
+            <h2 class="footer__ubicacion-title">Ubicacion de la compañia</h2>
+            <div class="footer__ubicacion-mapa">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3397.785530799882!2d-60.70065412352229!3d-31.61233600575293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b5075ffdbbaf65%3A0xd36dc736af578df7!2sSan%20Jer%C3%B3nimo%206011%2C%20Santa%20Fe%20de%20la%20Vera%20Cruz%2C%20Santa%20Fe!5e0!3m2!1ses-419!2sar!4v1686853975947!5m2!1ses-419!2sar"
+                    width="350" height="150" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </section>
+    </footer>
+</body>
+
+</html>
---
+++ includes/templates/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="/CSS/normalice.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/contacto.css">
+    <link rel="stylesheet" href="CSS/mediasqries.css">
+
+
+<body>
+    <header class="header">
+        <div class="contenido_head">
+            <a href="https://www.jdfinmuebles.com/"><img src="img/Logo-JDF-1.3.png" alt="logo-de-JDF-inmuebles"
+                    class="header__logo"></a>
+            <div class="head_mat">
+                <img src="img/logo-cci.png" alt="logo de cci santa fe">
+                <p>Mat:763</p>
+                <p>Mat:764</p>
+            </div>
+        </div>
+        <nav class="header_nav">
+            <a href="index.html" class="header__link">Inicio</a>
+            <a href="ventas.html" class="header__link">ventas</a>
+            <a href="alquileres.html" class="header__link">alquileres</a>
+            <a href="contacto.html" class="header__link">contacto</a>
+        </nav>
+    </header>
